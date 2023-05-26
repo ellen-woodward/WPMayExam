708cedc92847855fd7a3df68985ad21df1ab91ab
ellen-woodward <s00222321@mail.itsligo.ie>
2023-05-26 13:34:01 +0100

    updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { staffMember } from './interfaceStaff';
+import { MockapiService } from './services/mockapi.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'MayExam';
+
+  results!: staffMember[]
+
+  constructor(private _mockapi: MockapiService) { }
+
+  ngOnInit(): void {
+    this.getStaff();
+  }
+
+  getStaff(): boolean {
+    this._mockapi.getStaffDetails().subscribe(
+      staffData => {
+        this.results = staffData;
+        console.log(this.results);
+      }
+    )
+    return false;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MockapiService } from './services/mockapi.service';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [MockapiService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
